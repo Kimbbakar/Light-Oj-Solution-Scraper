9baa3794ef953e5a8974a05bc2a86cb97ca2551a
Kimbbakar <chowdhuryosman04@gmail.com>
2017-12-11 15:28:58 +0600

    login with input username and password

--- LightOj/LightOj/spiders/loj.py
+++ LightOj/LightOj/spiders/loj.py
@@ -5,7 +5,31 @@ import scrapy
 class LojSpider(scrapy.Spider):
     name = 'loj'
     allowed_domains = ['lightoj.com']
-    start_urls = ['http://lightoj.com/']
+    start_urls = ['http://lightoj.com/login_main.php']
+    submission_url = 'http://lightoj.com/volume_usersubmissions.php'
 
     def parse(self, response):
-        pass
+
+        self.username = raw_input("Username/Email : ")
+        self.userpass = raw_input("Password: ")
+ 
+
+        return scrapy.FormRequest.from_response(
+            response,
+            formdata = {
+                'myuserid':self.username,
+                'mypassword':self.userpass,
+            },
+            callback = self.after_login
+
+        )
+
+    def after_login(self,response):
+        
+        if 'login_main.php' in response.text:
+            print "Login failed!!!"
+            return
+
+        print "Login Successful !!!"
+
+
